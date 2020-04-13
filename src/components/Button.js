1c4ac467f2758e4134e3fe0a34cdd0e12408d2f8
VukasinVujic <vukasinvujic021@gmail.com>
2020-04-13 18:36:52 +0200

    Consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,15 +3,27 @@ import LanguageContext from '../contexts/LanguageContext';
 
 
 class Button extends React.Component {
-    
-    static contextType = LanguageContext;
+    // we use contextType for this.context(contextType must have this name)
+    // static contextType = LanguageContext;
+
+    renderSubmit(value) {
+        return value === 'english' ? 'Submit' : 'Voorleggen';
+    }
     
     render(){
-        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+        // for this.context 
+        // const te?xt = this.context === 'english' ? 'Submit' : 'Voorleggen';
+        
 
         return(
             <button className="ui button primary">
-                {text}
+                {/* for this.context  */}
+                {/* {text} */}
+
+                {/* value got from Provider */}
+                <LanguageContext.Consumer > 
+                    { value => this.renderSubmit(value)}
+                </LanguageContext.Consumer>
             </button>
         )
     }
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,12 @@
 import React from 'react';
 
+//  const lala = React.createContext('english');
+//  console.log(lala)
+// export default lala;
+
+
 export default React.createContext('english');
+
+ 
+
+
