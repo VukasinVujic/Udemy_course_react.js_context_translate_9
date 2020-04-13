0f8290db1d0b4d07a20b19eb3e0e6b2f556a1e0c
VukasinVujic <vukasinvujic021@gmail.com>
2020-04-13 12:00:47 +0200

    initial setup

---
+++ src/components/Field.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Field extends React.Component {
+    render(){
+        return(
+            <div className="ui field ">
+                <label htmlFor="">Name: </label>
+                <input type=" "/>
+
+            </div> 
+        )
+    }
+}
+
+export default Field; 
