53c1e1f4b78b26b607827674ba429e449cdd7776
VukasinVujic <vukasinvujic021@gmail.com>
2020-04-14 12:45:21 +0200

    1.replacing redux with context,change LanguageContext,app,LanguageSelector

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+// import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 import LanguageSelector from '../components/LanguageSelector';
 
 
 class App extends React.Component {
-
-    state = {
-        language: 'english'
-    };
-
-    onLanguageChange = language => {
-        this.setState({language:language})
-    };
+    // we remove the state & onLanguageChange because we moved buisnis logic to Language store
+    // state = { language: 'english'}; 
+    // onLanguageChange = language => {
+    //     this.setState({language:language})
+    // };
 
 
     render(){
+        // we put LanguageStore around everything so LanguageSelector would have access to the store
         return(
              <div className="ui container">
-                <LanguageSelector onLanguageChange={this.onLanguageChange}  />
-                <ColorContext.Provider value="red">
-                     <LanguageContext.Provider value={this.state.language}>
-                        <UserCreate />
-                    </LanguageContext.Provider>
-                </ColorContext.Provider>
+                <LanguageStore>
+                    {/* we remove onLanguageChange={this.onLanguageChange} from LanguageSelector
+                        since is not getting it from here but from LanguageStore
+                     */}
+                    <LanguageSelector />
+
+                    <ColorContext.Provider value="red">
+                        {/* <LanguageContext.Provider value={this.state.language}> */}
+                            <UserCreate />
+                        {/* </LanguageContext.Provider> */}
+                    </ColorContext.Provider>
+                </LanguageStore>
             </div>
         )
     }
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -4,9 +4,27 @@ import React from 'react';
 //  console.log(lala)
 // export default lala;
 
+ const Context = React.createContext('english');
 
-export default React.createContext('english');
+ export class LanguageStore extends React.Component {
+     
+    state={language: 'english'}
 
+    onLanguageChange = (language) => {
+        this.setState({language});
+    }
+
+    render(){
+        return(
+            // children is jsx from parent component
+            <Context.Provider value={{...this.state, onLanguageChange: this.onLanguageChange }}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+
+ } 
+
+ export default Context;
  
-
 
